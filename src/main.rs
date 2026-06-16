71e858dce457de16efe5a0a5f975cf961670cb24
Andrew Monteiro <andrew.agoravai@gmail.com>
2026-06-16 07:58:08 -0300

    feat: fetch OpenSky inside loop to create a simple polling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+use tokio::time::{Duration, sleep};
+
 mod model;
 mod opensky;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let data = opensky::fetch().await?;
+    loop {
+        match opensky::fetch().await {
+            Ok(data) => {
+                let count = data.states.unwrap_or_default().len();
+                println!("Found {} aircraft", count)
+            }
+            Err(err) => {
+                println!("Error: {}", err)
+            }
+        }
 
-    let count = data.states.unwrap_or_default().len();
-
-    println!("Found {} aircraft", count);
-
-    Ok(())
+        sleep(Duration::from_secs(15)).await;
+    }
 }
