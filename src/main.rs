48d6f340d0fad6ff2095223a5680afb30d2c3fc9
Andrew Monteiro <andrew.agoravai@gmail.com>
2026-06-18 05:59:43 -0300

    feat: start axum webserver in parallel to poller

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use tokio::time::{Duration, sleep};
+use axum::{Router, routing::get};
+use tokio::{
+    net::TcpListener,
+    time::{Duration, sleep},
+    try_join,
+};
 
 use crate::{event::Event, state::AircraftState};
 
@@ -9,6 +14,31 @@ mod state;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let app = Router::new().route("/health", get(health));
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await?;
+
+    let poller_result = tokio::spawn(async {
+        println!("starting poller");
+        run_poller().await
+    });
+
+    let server_result = tokio::spawn(async {
+        println!("starting web server");
+        axum::serve(listener, app).await
+    });
+
+    match try_join!(poller_result, server_result) {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err.into()),
+    }
+}
+
+async fn health() -> &'static str {
+    "OK"
+}
+
+async fn run_poller() -> anyhow::Result<()> {
     let mut previous = AircraftState::new();
 
     loop {
