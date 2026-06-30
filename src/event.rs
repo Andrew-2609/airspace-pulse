f90e2202c1da3ed73e96ed41e0d513be614754b4
Andrew Monteiro <andrew.agoravai@gmail.com>
2026-06-30 07:22:27 -0300

    feat(event): serialize on_ground field and add new 'Present' entry to enum

--- src/event.rs
+++ src/event.rs
@@ -1,9 +1,10 @@
 use serde::{Serialize, ser::SerializeStruct};
 
-use crate::model::{Aircraft, Position};
+use crate::model::Aircraft;
 
 #[derive(Clone)]
 pub enum AircraftEvent {
+    Present(Aircraft),
     Entered(Aircraft),
     Left(Aircraft),
     Landed(Aircraft),
@@ -17,14 +18,7 @@ impl AircraftEvent {
             Self::Left(_) => "left",
             Self::Landed(_) => "landed",
             Self::TookOff(_) => "took_off",
-        }
-    }
-
-    pub fn aircraft_position(&self) -> &Position {
-        match self {
-            Self::Entered(ac) | Self::Left(ac) | Self::Landed(ac) | Self::TookOff(ac) => {
-                &ac.position
-            }
+            Self::Present(_) => "present",
         }
     }
 }
@@ -35,18 +29,20 @@ impl Serialize for AircraftEvent {
         S: serde::Serializer,
     {
         match self {
-            AircraftEvent::Entered(ac)
-            | AircraftEvent::Left(ac)
-            | AircraftEvent::Landed(ac)
-            | AircraftEvent::TookOff(ac) => {
+            Self::Present(ac)
+            | Self::Entered(ac)
+            | Self::Left(ac)
+            | Self::Landed(ac)
+            | Self::TookOff(ac) => {
                 // Important: increase the state len if new fields are added
-                let mut state = serializer.serialize_struct("AircraftEvent", 6)?;
+                let mut state = serializer.serialize_struct("AircraftEvent", 7)?;
                 state.serialize_field("action", self.action())?;
                 state.serialize_field("icao24", &ac.icao24)?;
                 state.serialize_field("callsign", ac.callsign.as_deref().unwrap_or_default())?;
                 state.serialize_field("latitude", &ac.position.lat)?;
                 state.serialize_field("longitude", &ac.position.lon)?;
                 state.serialize_field("category", &ac.category)?;
+                state.serialize_field("on_ground", &ac.on_ground)?;
                 state.end()
             }
         }
